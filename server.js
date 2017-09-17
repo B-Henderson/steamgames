af1598696ecdaefd06be01faec6684c1fa76a8c6
Ben henderson <benhenderson976@gmail.com>
2017-09-17 00:16:55 +0000

    running off c9

--- server.js
+++ server.js
@@ -4,22 +4,35 @@ const app = express()
 const request = require('request');
 const bodyParser = require('body-parser');
 
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+
 app.route('/')
     .get(function(req, res) {
-        res.status(200).send('Hello World!')
+        res.status(200).send('Hello World!');
     })
     .post(function(req, res) {
+        let userid = req.body;
         request
-            .get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + apikey + '&include_appinfo=1&steamid=' + req.param('steamid') + '&format=json', function(err, response, body) {
+            .get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=' + apikey + '&include_appinfo=1&steamid=' + userid.steamid + '&format=json', function(err, response, body) {
+                
+                console.log(response.body, body);
+                if(err) throw err;
                 if (!err && response.statusCode === 200) {
                     let locals = JSON.parse(body);
-                    // res.send(body);
-                    let game = Math.floor(Math.random() * locals.response.games.length + 1);
-                    random = locals.response.games[game];
-                    randomGame = random;
-                    res.json(locals);
+                    step2(locals);
                 }
-            })
+            });
+            
+        var step2 = function(games){
+            step3(games);
+        }
+        var step3 = function(games){
+            res.json(games);
+        }
     })
 
 
