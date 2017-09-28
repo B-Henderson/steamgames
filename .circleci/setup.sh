6cc405bbd4d5d5f62e09f205893ab10dcf5767cb
b-henderson <benhenderson976@gmail.com>
2017-09-29 00:56:54 +0100

    circleci

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -7,11 +7,11 @@
      
   cat > ~/.netrc << EOF
   machine api.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
+    login $heroku_name
+    password $heroku_ap
   machine git.heroku.com
-    login $HEROKU_LOGIN
-    password $HEROKU_API_KEY
+    login $heroku_name
+    password $heroku_ap
   EOF
 
   # Add heroku.com to the list of known hosts
