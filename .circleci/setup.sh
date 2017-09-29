b425857062478368bd690df65a305da92236aed6
b-henderson <benhenderson976@gmail.com>
2017-09-29 18:27:02 +0100

    tests

--- .circleci/setup.sh
+++ .circleci/setup.sh
@@ -5,14 +5,14 @@
   sudo tar -xvzf heroku-linux-amd64.tar.gz -C /usr/local/lib
   sudo ln -s /usr/local/lib/heroku/bin/heroku /usr/local/bin/heroku
      
- cat << EOF >> ~/.netrc
+cat > ~/.netrc <<EOF
   machine api.heroku.com
     login ${herokuName}
     password ${heroku_ap}
   machine git.heroku.com
     login ${herokuName}
     password ${heroku_ap}
-  EOF
+EOF
 
   # Add heroku.com to the list of known hosts
   ssh-keyscan -H heroku.com >> ~/.ssh/known_hosts
